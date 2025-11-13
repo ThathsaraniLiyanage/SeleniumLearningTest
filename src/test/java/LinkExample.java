c91e8a381042f069fa02500747b3ddc199144781
ThathsaraniLiyanage <tliyanage007@gmail.com>
2025-11-13 20:21:41 +0530

    Up to find my destination part

---
+++ src/test/java/LinkExample.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LinkExample {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openLinkTestPage(){
+
+        //To open the browser
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.leafground.com/link.xhtml");
+
+    }
+
+
+    @Test
+    public void LinkTests(){
+
+        //01- Take me to Dashboard
+        WebElement homelink = driver.findElement(By.linkText("Go to Dashboard"));
+        homelink.click();
+        driver.navigate().back();
+
+        //02- Find my destination
+        WebElement wheretoGo = driver.findElement(By.partialLinkText("Find the URL"));
+        String path = wheretoGo.getAttribute("href");
+        System.out.println("This link is going to : " + path);
+
+        //03- Am I broken link?
+        
+        //04- Duplicate Link
+        //05- Count page Link
+        //06- count layout links
+
+    }
+
+
+
+}
